6f9fbaee40d066ffb37053d667168cdbd1700ecd
Adriano Song <adrianosong@hotmail.com>
2020-09-18 12:04:40 -0300

    add function for dismiss keyboard

---
+++ Sources/MyExtensions/UIApplication+Extension.swift
@@ -0,0 +1,14 @@
+//
+//  UIApplication+Extension.swift
+//  
+//  Created by Song on 18/09/20.
+//
+
+import Foundation
+
+extension UIApplication {
+    /// extension for dismiss keyboard (howToUse: UIApplication.shared.endEditing())
+    func endEditing() {
+        sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+}
